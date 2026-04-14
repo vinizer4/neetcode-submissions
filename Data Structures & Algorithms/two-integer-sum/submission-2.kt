df791eabd7eede78b0fb3d1c984b1d54c50f47db
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2026-04-14 17:16:32 -0300

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val valueToIndex = hashMapOf<Int, Int>()
+
+        for (index in nums.indices) {
+            val currentValue = nums[index]
+            val complementValue = target - currentValue
+
+            if (valueToIndex.containsKey(complementValue)) {
+                val complementIndex = valueToIndex[complementValue]!!
+                return intArrayOf(complementIndex, index)
+            }
+
+            valueToIndex[currentValue] = index
+        }
+
+        return intArrayOf()
+    }
+}
