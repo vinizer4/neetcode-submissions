59c339afc636a6cb58e8be5cd1d248401c872b25
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2026-04-15 10:48:05 -0300

    Add: concatenation-of-array - submission-3

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-3.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun getConcatenation(nums: IntArray): IntArray {
+        val size = nums.size
+
+        val result = IntArray(2 * size)
+
+        for (index in nums.indices) {
+            result[index] = nums[index]
+            result[index + size] = nums[index]
+        }
+
+        return result
+    }
+}
