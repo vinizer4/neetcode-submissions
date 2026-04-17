3e9586b4539ece44ead00c6dc4a31844efa72d5f
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2026-04-16 23:37:36 -0300

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.kt
@@ -0,0 +1,26 @@
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        if (strs.isEmpty()) return ""
+
+        val referenceString = strs[0]
+
+        for (referenceCharPosition in referenceString.indices) {
+
+            val referenceChar = referenceString[referenceCharPosition]
+
+            for (comparisonStringIndex in 1 until strs.size) {
+
+                val comparisonString = strs[comparisonStringIndex]
+
+                if (
+                    referenceCharPosition >= comparisonString.length ||
+                    comparisonString[referenceCharPosition] != referenceChar
+                ) {
+                    return referenceString.substring(0, referenceCharPosition)
+                }
+            }
+        }
+
+        return referenceString
+    }
+}
