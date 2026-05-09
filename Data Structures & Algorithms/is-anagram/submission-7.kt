e3dafbacae7067cb5bc3ca9cca7569f6dd57788a
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2026-05-09 16:00:17 -0300

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+
+        if (s.length != t.length) return false
+
+        val countS = HashMap<Char, Int>()
+        val countT = HashMap<Char, Int>()
+
+        for (char in s) {
+            val count = countS.getOrDefault(char, 0) + 1
+            countS[char] = count
+        }
+
+        for (char in t) {
+            val count = countT.getOrDefault(char, 0) + 1
+            countT[char] = count
+        }
+
+        return countS == countT
+    }
+}
